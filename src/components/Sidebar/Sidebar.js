2dfc0268fe2bde72c477451a46e66914083e7dda
Matthew Peldunas <peldunasma@gmail.com>
2024-05-18 15:17:41 -0400

    logout not working

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,17 +1,42 @@
 import React from "react";
-import avatar from "../../images/Avatar.svg"
-import "./Sidebar.css"; 
+import "./Sidebar.css";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
+import { useContext } from "react";
+import { NavLink } from "react-router-dom";
 
-function SideBar() {
-    return (
-        <div className="profile__logo">
-            <img
-                src={avatar}
-                alt="profile__avatar"
-                className="profile__avatar-image"
-            />
-            <p className="profile__avatar-name">Terrence Tegegne</p>
-        </div>
-    );
+function SideBar(setIsLoggedIn) {
+  const navigate = NavLink();
+  const currentUser = useContext(CurrentUserContext);
+  return (
+    <div className="sidebar">
+      <div className="sidebar__container">
+        <img
+          src={currentUser?.avatar}
+          alt="sidebar__avatar"
+          className="sidebar__avatar-image"
+        />
+        <p className="sidebar__avatar-name">
+            {currentUser?.name}
+        </p>
+      </div>
+      <button 
+      className="sidebar__edit-data"
+      type="text"
+      >
+        Change profile data
+      </button>
+      <button 
+      className="sidebar__logout"
+      type="text"
+      onClick={() => {
+        navigate("/");
+        setIsLoggedIn(false);
+        localStorage.removeItem("jwt");
+      }}
+      >
+        Log out
+      </button>
+    </div>
+  );
 }
-export default SideBar; 
+export default SideBar;
