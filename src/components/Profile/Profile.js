e0239e3bd5c983a8703b320453fff1c4e09c8f06
Matthew Peldunas <peldunasma@gmail.com>
2024-05-31 18:06:54 -0400

    refactored code

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,10 +2,8 @@ import React from "react";
 import "./Profile.css";
 import SideBar from "../Sidebar/Sidebar";
 import ClothesSection from "../ClothesSection/ClothesSection";
-import { useContext } from "react";
-import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
-const Profile = ({
+function Profile ({
   onSelectCard,
   handleCreateModal,
   clothingItems,
@@ -13,21 +11,13 @@ const Profile = ({
   handleEditProfileModal,
   onCardLike,
   isLoggedIn,
-}) => {
-  const { currentUser } = useContext(CurrentUserContext);
-
-  const profileCards = clothingItems.filter(
-    (item) => item.owner === currentUser?._id
-  );
-
+}) {
   return (
     <div className="profile">
       <SideBar
         setIsLoggedIn={setIsLoggedIn}
         handleEditProfile={handleEditProfileModal}
       />
-      {profileCards.map((item) => {
-       return ( 
       <ClothesSection
         onSelectCard={onSelectCard}
         handleCreateModal={handleCreateModal}
@@ -35,8 +25,6 @@ const Profile = ({
         onCardLike={onCardLike}
         isLoggedIn={isLoggedIn}
       />
-       );
-    })} 
     </div>
   );
 };
