218ea1f57c19a6296cf8f28a67cd75a2b9378b95
Matthew Peldunas <peldunasma@gmail.com>
2024-06-24 14:02:02 -0400

    new

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,7 @@
-export const baseUrl = "http://localhost:3001";
+export const baseUrl =
+process.env.NODE_ENV === "production"
+  ? "https://api.wtwr.xseller.com"
+  : "http://localhost:3001";
 
 export const checkResponse = (res) => {
   return res.ok ? res.json() : Promise.reject(`Error ${res.status}`);
